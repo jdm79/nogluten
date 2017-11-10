87cf53db05707cbbd33c5b572bd9daf91c3fb77f
jdm79 <james.malvern@gmail.com>
2017-11-10 19:15:33 +0000

    add basic js, css and html files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// static coords for location on map
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}

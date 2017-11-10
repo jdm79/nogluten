947f144094fdcb4acf49e1355fd5c9a3917fe25d
jdm79 <james.malvern@gmail.com>
2017-11-10 19:58:06 +0000

    start map in the US, then add function to find show current position

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 // static coords for location on map
 var map;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 24, lng: -110.1},
+    zoom: 10
+  });
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: position.coords.latitude, lng: position.coords.longitude},
+    zoom: 15
   });
 }

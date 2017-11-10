aaf4032a97b18533390a6067943169ddc4e55581
jdm79 <james.malvern@gmail.com>
2017-11-10 20:28:15 +0000

    use onload() to show current pos on map

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
 // static coords for location on map
 var map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 24, lng: -110.1},
-    zoom: 10
-  });
-}
+
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(initMap);
     } else {
         alert("Geolocation is not supported by this browser.");
     }
 }
 
-function showPosition(position) {
+
+function initMap(pos) {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: position.coords.latitude, lng: position.coords.longitude},
+    center: {lat: pos.coords.latitude, lng: pos.coords.longitude},
     zoom: 15
   });
 }

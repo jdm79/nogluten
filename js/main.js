6891bd7d840162d979221dd62621d0d3ba0d8bf9
jdm79 <james.malvern@gmail.com>
2017-11-11 13:09:01 +0000

    add array of markers

--- js/main.js
+++ js/main.js
@@ -14,8 +14,32 @@ function initMap(pos) {
       center: {lat: pos.coords.latitude, lng: pos.coords.longitude},
       zoom: 15
     });
+
   let marker = new google.maps.Marker({
       position: {lat: pos.coords.latitude, lng: pos.coords.longitude},
       map: map
   });
+
+  // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+  // Add a marker clusterer to manage the markers.
+   var markerCluster = new MarkerClusterer(map, markers,
+       {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
+
+
+var locations = [
+        {lat: 51.507, lng: -0.055},
+        {lat: 51.514, lng: -0.0549},
+        {lat: 51.512, lng: -0.055},
+        {lat: 51.509, lng: -0.0558},
+        {lat: 51.508, lng: -0.059},
+
+      ]
